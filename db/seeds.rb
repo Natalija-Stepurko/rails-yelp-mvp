a37a730c25e796bb4b84c8451a15f9cfc61004b1
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-02-16 15:48:23 +0000

    Added controllers and views. The app is working, but no refactoring implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  puts "creating restaurant"
+  res = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Restaurant::CATEGORIES.sample
+  )
+  puts "finished creating #{res.id} restaurant"
+end
